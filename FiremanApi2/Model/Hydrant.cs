2638174feb070e9c8468a505c60efce80e7b73b4
andreygolubkow <andreygolubkow@gmail.com>
2018-02-07 00:07:52 +0700

    Добавил поля в гидрант. Изменил контроллер гидранта.

--- FiremanApi2/Model/Hydrant.cs
+++ FiremanApi2/Model/Hydrant.cs
@@ -13,23 +13,41 @@ namespace FiremanApi2.Model
     [Table("hydrants")]
     public class Hydrant : IMapObject
     {
-        public Hydrant()
-        {
-            GpsPoint = new GpsPoint();
-        }
-
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
         [Column("id")]
         [Key]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Место, где находится гидрант. Например, адрес.
+        /// </summary>
+        [Column("place")]
+        public string Place { get; set; }
         
+        /// <summary>
+        /// Дата проверки гидранта.
+        /// </summary>
         [Column("revision_date")]
         public DateTime RevisionDate { get; set; }
 
+        /// <summary>
+        /// Активен гидрант или нет.
+        /// </summary>
         [Column("active")]
         public bool Active { get; set; }
+        
+        /// <summary>
+        /// Характер неисправности.
+        /// </summary>
+        [Column("fault_problem")]
+        public string FaulProblem { get; set; }
 
-        public GpsPoint GpsPoint { get; set; }
+        /// <summary>
+        /// Координаты гидранта.
+        /// </summary>
+        public GpsPoint GpsPoint { get; set; } = new GpsPoint();
 
         /// <summary>
         /// Ответственный за гидрант.
@@ -37,8 +55,17 @@ namespace FiremanApi2.Model
         [Column("responsible")]
         public string Responsible { get; set; }
 
+        /// <summary>
+        /// Дополнительное описание.
+        /// </summary>
         [Column("descr")]
         public string Description { get; set; }
+        
+        /// <summary>
+        /// Вид противопожарного водоснобжения. Гидрант, водоём или пирс.
+        /// </summary>
+        [Column("water_type")]
+        public string WaterType { get; set; }
 
     }
 }
